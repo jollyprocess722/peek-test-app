eed9099e2cf04c395fa8010cee0840f8ff22b6e0
jollyprocess722 <jollyprocess722@agentmail.to>
2026-09-13 04:14:57 +0000

    Change server listen address to '0.0.0.0'

--- server.js
+++ server.js
@@ -88,6 +88,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(PORT, '127.0.0.1', () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`wave3a test app listening on ${PORT}`);
 });
